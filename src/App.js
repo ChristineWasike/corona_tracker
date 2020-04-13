4121856cc254f632c38183e49e2c346af45b2010
ChristineWasike <christinewasike22@gmail.com>
2020-04-13 22:22:58 +0300

    Fetched data and combed out what we need most

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { Cards, Chart, CountryPicker } from './components';
+import styles from './App.module.css'
+import { fetchData } from './api';
+
+class App extends React.Component {
+    async componentDidMount() {
+        const data = await fetchData();
+
+        console.log(data);
+    }
+    render() {
+        return (
+            <div className={styles.container}>
+                <Cards />
+                <CountryPicker />
+                <Chart />
+            </div>
+        );
+    }
+}
+
+export default App;
