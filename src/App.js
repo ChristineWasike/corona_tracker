0a354acf5ede3a626f889d79b4ce0ef3f0b29eae
ChristineWasike <christinewasike22@gmail.com>
2020-04-13 23:55:49 +0300

    Incoperating material ui for the components bein displayed

--- src/App.js
+++ src/App.js
@@ -5,15 +5,21 @@ import styles from './App.module.css'
 import { fetchData } from './api';
 
 class App extends React.Component {
-    async componentDidMount() {
-        const data = await fetchData();
-
-        console.log(data);
+    state = {
+        data:{},
     }
+
+    async componentDidMount() {
+        const fetchedData = await fetchData();
+
+        this.setState({data:fetchedData})
+    }
+
     render() {
+        const {data} = this.state
         return (
             <div className={styles.container}>
-                <Cards />
+                <Cards data = {data}/>
                 <CountryPicker />
                 <Chart />
             </div>
